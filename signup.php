287ad77f9341f7f9556b68b17c5cd45f71cbfae4
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-12-02 22:46:37 +0530

    signup files

---
+++ signup.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+	<link rel="stylesheet" type="text/css" href="css/signup.css">
+</head>
+<body>
+	<div class="body">
+		<form action="" method="post">
+			<fieldset class="crd">
+				<legend><h2>SIGN UP</h2></legend>
+				<label for="first_name">First Name</label>
+				<input type="text" name="first_name" id="first_name" placeholder="Enter First Name"><br>
+
+				<label for="last_name">Last Name</label>
+				<input type="text" name="last_name" id="last_name" placeholder="Enter Last Name"><br>
+
+				<label for="email">Email</label>
+				<input type="email" name="email" id="email" placeholder="Enter Email"><br>
+
+				<label for="tel">Telephone</label>
+				<input type="tel" name="tel" id="tel" placeholder="Enter Telephone"><br>
+
+				<label for="dob">Date Of Birth</label>
+				<input type="Date" name="dob" id="dob" placeholder="Enter Date Of Birth"><br>
+
+				<label for="password">Password</label>
+				<input type="password" name="password" id="password" placeholder="Enter Password"><br>
+
+				<button class="btn" type="submit" id="submit">Register</button>
+				<p>Already have an account<a href="login.php">Log In</a></p>
+
+				
+			</fieldset>
+		</form>
+	</div>
+
+</body>
+</html>
