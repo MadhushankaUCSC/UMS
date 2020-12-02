7a3f31cdf57c5029c99e259e41a06b1c5d59ad60
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-12-02 21:55:52 +0530

    login files

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Login</title>
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+	<div class="background">
+		<div class="card">
+			<form method="post" action="" >
+				<fieldset>
+					<legend><h2>LOGIN</h2></legend>
+					<label for="email">Email</label>
+					<input type="email" name="email" id="email"  placeholder="Enter Email">
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password" placeholder="Enter Password">
+					<button  class="btn" type="submit" id="submit">Log In</button><br>
+				</fieldset>
+			</form>
+		</div>
+	</div>
+
+</body>
+</html>
