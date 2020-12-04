e7e6e768bbc91c28bf3ca989d853bd7cbadbd350
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-12-04 18:53:53 +0530

    modified

--- home.php
+++ home.php
@@ -10,8 +10,8 @@
 <body>
 
 <div class="header">
-  <h1>My Website</h1>
-  <p>A <b>responsive</b> website created by me.</p>
+  <h1>Simple User Management System</h1>
+  <p>website created by me.</p>
 </div>
 
 <div class="navbar">
