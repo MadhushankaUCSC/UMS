c8afe6a7924a4d524015d5d2a6012e5efca6a59f
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-12-07 10:45:39 +0530

    modify login

--- login.php
+++ login.php
@@ -15,11 +15,11 @@
 					<legend><h2>LOGIN</h2></legend>
 					<label for="email">Email</label><!--email lable-->
 					<input type="email" name="email" id="email"  placeholder="Enter Email" value="<?php if($_GET['user_email']=='ww'){
-						echo '';}else{echo base64_decode($_GET['user_email']) ;} ?>"><!-- input email box-->
+						echo '';}else{echo base64_decode($_GET['user_email']) ;} ?>" required=""><!-- input email box-->
  
 					<label for="password">Password</label><!--password lable-->
 					<input type="password" name="password" id="password" placeholder="Enter Password" value="<?php if($_GET['user_email']=='ww'){
-						echo '';}else{echo base64_decode($_GET['user_password']) ;} ?>" ><!-- input password box-->
+						echo '';}else{echo base64_decode($_GET['user_password']) ;} ?>" required=""><!-- input password box-->
 
 					<button  class="btn" type="submit" name="submit" id="submit">Log In</button><br>
 				</fieldset>
