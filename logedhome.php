14965525891f2745c4b570f1ebe0714fbbfe696f
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-12-07 10:43:30 +0530

    modify

--- logedhome.php
+++ logedhome.php
@@ -8,10 +8,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 	<title>Welcome</title>
 	<link rel="stylesheet" type="text/css" href="css/home.css">
 </head>
 <body>
+	<div><a href="login.php?user_email=ww" style="float: right;color: orange;padding: 30px;">Log out</a></div>
 	<div class="header1">
   <h1>Welcome &ensp;<?php echo $_SESSION['first_name'];?></h1>
  
